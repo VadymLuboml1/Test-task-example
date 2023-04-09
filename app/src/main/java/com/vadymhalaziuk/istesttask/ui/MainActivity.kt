735b3c803479259c4ea94f8a0a7d20cfdeae1970
vadym_halaziuk <galazjukvadim@gmail.com>
2023-04-09 20:10:44 +0300

    removed useless code

--- app/src/main/java/com/vadymhalaziuk/istesttask/ui/MainActivity.kt
+++ app/src/main/java/com/vadymhalaziuk/istesttask/ui/MainActivity.kt
@@ -1,30 +1,13 @@
 package com.vadymhalaziuk.istesttask.ui
 
-import android.Manifest
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.MaterialTheme
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val notificationStatus = MutableSharedFlow<Boolean>()
-
-    private val permissionListener = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) {
-        lifecycleScope.launch {
-            notificationStatus.emit(it)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +16,6 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 ActionsComposition()
             }
-        }
-    }
-
-    private suspend fun requestAndGetNotificationPermission(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            permissionListener.launch(Manifest.permission.POST_NOTIFICATIONS)
-
-            notificationStatus.first()
-        } else {
-            true
         }
     }
 
