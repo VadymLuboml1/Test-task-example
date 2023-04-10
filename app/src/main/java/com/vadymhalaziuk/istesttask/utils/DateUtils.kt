33328a9ec549ce8d2ada6724f108929392ef2f05
vadym_halaziuk <galazjukvadim@gmail.com>
2023-04-10 12:13:26 +0300

    fixed days diff with BE

--- app/src/main/java/com/vadymhalaziuk/istesttask/utils/DateUtils.kt
+++ app/src/main/java/com/vadymhalaziuk/istesttask/utils/DateUtils.kt
@@ -9,10 +9,12 @@ fun currentTimeStamp(): Long =
 /**
  * Return day of week starting from Sunday
  * */
-fun currentDayOfWeek(): DayOfWeek =
+fun currentDayOfWeek(stepBack: Int = SYSTEM_CALENDAR_DAYS_DIFF_WITH_BE): DayOfWeek =
     requireNotNull(
         DayOfWeek.values()
             .find {
-                it.ordinal == Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+                it.ordinal == Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - stepBack
             }
     )
+
+private const val SYSTEM_CALENDAR_DAYS_DIFF_WITH_BE = 1
